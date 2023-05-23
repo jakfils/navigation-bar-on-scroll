220c68370e7d5b5e3e464567f4ef000cc4de7691
jakfils <jakfilstechno@gmail.com>
2023-05-23 21:13:21 +0100

    Fix mobile view

--- js/index.js
+++ js/index.js
@@ -3,6 +3,10 @@ const hamburger1 = document.getElementById("hamburger1");
 const hamburger2 = document.getElementById("hamburger2");
 const mobileNav = document.getElementById("mobileNav");
 const mediaQuery = window.matchMedia("(min-width: 768px)");
+const mediaQuery2 = window.matchMedia("(max-width: 768px)");
+const container = document.querySelector(".container");
+const nav = document.querySelector("nav");
+const main = document.querySelector(".main");
 
 hamburgers.forEach((hamburger) => {
   hamburger.addEventListener("click", (e) => {
@@ -10,6 +14,13 @@ hamburgers.forEach((hamburger) => {
     hamburger2.classList.toggle("invisible");
     hamburger2.classList.toggle("visible");
     mobileNav.classList.toggle("invisible");
+    container.insertBefore(nav, container.firstChild);
+    if (mobileNav.classList.contains("invisible")) {
+      main.style.top = "0px";
+    } else {
+      main.style.top = "200px";
+    }
+    nav.style.zIndex = "4";
   });
 });
 
@@ -19,4 +30,9 @@ mediaQuery.addEventListener("change", (e) => {
   hamburger2.classList.add("invisible");
   hamburger2.classList.remove("visible");
   mobileNav.classList.add("invisible");
+});
+
+mediaQuery2.addEventListener("change", (e) => {
+  main.style.top = "0px";
+  nav.style.zIndex = "4";
 });
